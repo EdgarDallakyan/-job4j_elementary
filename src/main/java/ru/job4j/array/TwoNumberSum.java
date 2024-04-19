33d259d60f46b78f6050db00dfb8b852eac791eb
edgar <edgar_dallakyan@mail.ru>
2024-04-19 15:25:21 +0300

    Codium. Нейросети спешат на помощь. [#505152]

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -2,6 +2,9 @@ package ru.job4j.array;
 
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
+        if (array == null || array.length < 2) {
+            throw new IllegalArgumentException("Массив не должен быть null и должен содержать как минимум два элемента.");
+        }
         int[] result = {};
         int i = 0;
         int j = array.length - 1;
