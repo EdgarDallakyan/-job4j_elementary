c689aa6e4687adc812cbf26a6921a0e73f09866c
edgar <edgar_dallakyan@mail.ru>
2024-05-25 14:37:30 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -6,13 +6,13 @@ public class SortSelected {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexInRange(data, min, i, data.length - 1);
-            SwitchArray.swap(data, i, index);
+            data = SwitchArray.swap(data, i, index);
         }
         return data;
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{3, 1, 2, 5, 4};
+        int[] nums = new int[]{3, 1, 5, 2, 4};
         int[] rsl = sort(nums);
         for (int index = 0; index < rsl.length; index++) {
             System.out.println(rsl[index]);
